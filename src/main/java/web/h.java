1e99dfa0919f65c8537e8483eadb1edc3fef7bcd
Divya10 <divya.agarwal24@outlook.com>
2017-01-01 21:59:04 +0530

    commit

--- src/main/java/web/h.java
+++ src/main/java/web/h.java
@@ -17,7 +17,7 @@ public class h extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+  
 	}
 
 	/**
@@ -25,7 +25,7 @@ public class h extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		
 	}
 
 }
