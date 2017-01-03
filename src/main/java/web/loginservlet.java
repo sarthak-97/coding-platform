0b50dbdd57be87b09b27bab521a8fe1198083b19
sarthak-97 <sarthakgupta115@gmail.com>
2017-01-03 19:58:08 +0530

    login api ready with hibernate

--- src/main/java/web/loginservlet.java
+++ src/main/java/web/loginservlet.java
@@ -6,6 +6,14 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletResponse;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.javabrain.userdet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 
@@ -13,34 +21,83 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
  * Servlet implementation class loginservlet
  */
 @WebServlet(urlPatterns = {"/login"})
+@Repository
 public class loginservlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
+	
     public loginservlet() {
         super();
         // TODO Auto-generated constructor stub
   }
 
-        @Override
+       @Autowired
+	
+
+		@Override
         protected void doPost (HttpServletRequest req,
                             HttpServletResponse resp)
                             throws ServletException, IOException {
 
-            resp.setContentType("text/html");
+    
 
             try {
                 String idToken = req.getParameter("t2");
                 GoogleIdToken.Payload payLoad = IdTokenVerifierAndParser.getPayload(idToken);
                 String name = (String) payLoad.get("name");
                 String email = payLoad.getEmail();
+                String avatar= (String) payLoad.get("picture");
                 System.out.println("User name: " + name);
                 System.out.println("User email: " + email);
-
-               
-
+                System.out.println("avatar" + avatar);
+                SessionFactory sessionFactory =  new Configuration().configure().buildSessionFactory();
+                Session session =	sessionFactory.openSession();
+      	         session.beginTransaction();
+      	       Query queryResult = session.createQuery("from userdet");
+    	       java.util.List allUsers;
+    	       String pa,na;
+    	       
+    	       allUsers = queryResult.list();
+    	       int f;
+    	       f=0;
+    	       for (int i = 0; i < allUsers.size(); i++) {
+    	        userdet user = (userdet) allUsers.get(i);
+    	        pa=user.getEmailid();
+    	        na=user.getName();
+    	        if(email.equals(pa)){
+    	         f=1;
+    	         break; 
+    	         }
+    	        }
+    	         
+    	           if(f!=1){
+    	        	   
+    	        	   
+    	        userdet user= new userdet();  
+    	      user.setName(name);
+    	      user.setEmailid(email);
+    	      user.setAvatar(avatar);
+    	      session.save(user);
+    	      session.getTransaction().commit();
+    	      session.close();
+    	      user=null;
+    	       System.out.println(name);
+    	       req.setAttribute("t4", name);
+		       req.getRequestDispatcher("/profile.jsp").forward(req, resp);
+	         
+    	}
+    	           else
+    	           {   System.out.println("duplicate");
+    	         
+			       req.setAttribute("t4", name);
+			       req.getRequestDispatcher("/profile.jsp").forward(req, resp);
+    	         
+    	           }
+    	
+      	      
             } catch (Exception e) {
                 throw new RuntimeException(e);
             }
--- src/main/webapp/profile.jsp
+++ src/main/webapp/profile.jsp
@@ -11,34 +11,6 @@
 <div align=right>
 <nav>
 <a href="quespage.jsp#ques">ques page</a>
-<a href=#>edit profile</a>
-<a href=#>successful submission</a>
-<a href=#>logout</a>
-</nav>
-</div>
-<div>
-<form name=frm action="hello">
-<%= request.getParameter("t2") %> 
-<br>
-enter your name<input type=text name=t1><br>
-enter your admno<input type=text name=t2><br>
-enter you year<input type=text name=t3><br>
-<input type="submit">
-</form>
-</div>
-</body>
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
-</head>
-<body>
-<div align=right>
-<nav>
-<a href="quespage.jsp#ques">ques page</a>
 <a href=profile.jsp>edit profile</a>
 <a href=#>successful submission</a>
 <a href=#>logout</a>
@@ -46,9 +18,9 @@ enter you year<input type=text name=t3><br>
 </div>
 <div>
 <form name=frm action="hello">
-<%= request.getParameter("user_id") %>
+
 <br>
-enter your name<input type=text name=t1 value="<%= request.getParameter("user_id") %>"><br>
+enter your name<input type=text name=t1 value="<%= request.getAttribute("t4") %>"><br>
 enter your admno<input type=text name=t2><br>
 enter you year<input type=text name=t3><br>
 <input type="submit">
